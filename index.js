0c4a7045659caa9981721e7ddbf46652c535e8ef
Lee Kah Kin <kahkinlee998@gmail.com>
2023-10-03 10:57:35 +0000

    Add a badminton route

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/badminton', (req, res) => {
+  res.send(`Let's get fit!!`)
+})
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
